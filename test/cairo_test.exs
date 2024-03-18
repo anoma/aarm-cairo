78f29b5419c43adfcde6b28c08b2041f12732ee0
Song Xuyang <xuyangsong@hotmail.com>
2024-03-18 17:40:09 +0800

    add cairo_prove and cairo1 test

--- test/cairo_test.exs
+++ test/cairo_test.exs
@@ -1,6 +1,8 @@
 defmodule NifTest do
   use ExUnit.Case
+
   doctest Cairo.Cairo0
+  doctest Cairo.Cairo1
 
   alias Cairo.Cairo0
   alias Cairo.Cairo1
@@ -12,10 +14,14 @@ defmodule NifTest do
   end
 
   test "cairo1_api_test" do
-    {_trace, _memory} =
+    {trace, memory} =
       Cairo1.cairo1_vm_runner(
         "./native/cairo1/sierra_program",
         "2 [1 2 3 4] 0 [9 8]"
       )
+
+    # Prove and verify
+    {proof, public_input} = Cairo0.cairo_prove(trace, memory)
+    assert true = Cairo0.cairo_verify(proof, public_input)
   end
 end
