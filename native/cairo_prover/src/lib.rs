f506636b19ae46b65776237ec1be62e8c14e20cc
Xuyang Song <xuyangsong@hotmail.com>
2024-07-02 19:06:31 +0800

    add memory_segments to public inputs

--- native/cairo_prover/src/lib.rs
+++ native/cairo_prover/src/lib.rs
@@ -1,5 +1,5 @@
 use cairo_platinum_prover::{
-    air::{generate_cairo_proof, verify_cairo_proof, PublicInputs},
+    air::{generate_cairo_proof, verify_cairo_proof, PublicInputs, Segment, SegmentName},
     cairo_mem::CairoMemory,
     execution_trace::build_main_trace,
     register_states::RegisterStates,
@@ -18,10 +18,10 @@ fn cairo_prove(trace: Vec<u8>, memory: Vec<u8>, public_input: Vec<u8>) -> (Vec<u
     // Handle public inputs
     let rc_min = u16::from_le_bytes(public_input[0..2].try_into().unwrap());
     let rc_max = u16::from_le_bytes(public_input[2..4].try_into().unwrap());
-    let mem_len = u64::from_le_bytes(public_input[4..12].try_into().unwrap());
+    let mem_len = u64::from_le_bytes(public_input[4..12].try_into().unwrap()) as usize;
     let mut public_memory: hashbrown::HashMap<Felt252, Felt252> = HashMap::new();
     for i in 0..mem_len {
-        let start_index = (12 + i * 40) as usize;
+        let start_index = 12 + i * 40;
         let addr = Felt252::from(u64::from_le_bytes(
             public_input[start_index..start_index + 8]
                 .try_into()
@@ -36,6 +36,33 @@ fn cairo_prove(trace: Vec<u8>, memory: Vec<u8>, public_input: Vec<u8>) -> (Vec<u
         public_memory.insert(addr, value);
     }
 
+    let memory_segments_len = public_input[12 + 40 * mem_len] as usize;
+    let mut memory_segments = HashMap::new();
+    for i in 0..memory_segments_len {
+        let start_index = 12 + 40 * mem_len + 1 + i * 17;
+        let segment_type = match public_input[start_index] {
+            0u8 => SegmentName::RangeCheck,
+            1u8 => SegmentName::Output,
+            2u8 => SegmentName::Program,
+            3u8 => SegmentName::Execution,
+            4u8 => SegmentName::Ecdsa,
+            5u8 => SegmentName::Pedersen,
+            _ => continue, // skip unknown type
+        };
+
+        let segment_begin = u64::from_le_bytes(
+            public_input[start_index + 1..start_index + 9]
+                .try_into()
+                .unwrap(),
+        );
+        let segment_stop = u64::from_le_bytes(
+            public_input[start_index + 9..start_index + 17]
+                .try_into()
+                .unwrap(),
+        );
+        memory_segments.insert(segment_type, Segment::new(segment_begin, segment_stop));
+    }
+
     let num_steps = register_states.steps();
     let mut pub_inputs = PublicInputs {
         pc_init: Felt252::from(register_states.rows[0].pc),
@@ -45,7 +72,7 @@ fn cairo_prove(trace: Vec<u8>, memory: Vec<u8>, public_input: Vec<u8>) -> (Vec<u
         ap_final: Felt252::from(register_states.rows[num_steps - 1].ap),
         range_check_min: Some(rc_min),
         range_check_max: Some(rc_max),
-        memory_segments: HashMap::new(),
+        memory_segments,
         public_memory,
         num_steps,
     };
