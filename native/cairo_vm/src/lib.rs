2ea02554c5bc6cbca188dad65e62443ac7887bfb
carlo <carlo.modica@protonmail.com>
2024-07-29 15:08:50 +0200

    error handling from Juvix

--- native/cairo_vm/src/lib.rs
+++ native/cairo_vm/src/lib.rs
@@ -1,6 +1,6 @@
 use juvix_cairo_vm::{anoma_cairo_vm_runner, program_input::ProgramInput};
 use std::collections::HashMap;
-use rustler::NifResult;
+use rustler::{NifResult, Error};
 
 #[rustler::nif(schedule = "DirtyCpu")]
 fn cairo_vm_runner(program_content: String, inputs: String) -> NifResult<(String, Vec<u8>, Vec<u8>, Vec<u8>)> {
@@ -11,8 +11,12 @@ fn cairo_vm_runner(program_content: String, inputs: String) -> NifResult<(String
         ProgramInput::from_json(&inputs).unwrap()
     };
 
-    // TODO: handle the error
-    Ok(anoma_cairo_vm_runner(&program_content.as_bytes(), program_input).unwrap())
+    match anoma_cairo_vm_runner(&program_content.as_bytes(), program_input) {
+        Ok(result) => Ok(result),
+        Err(e) => {
+            Err(Error::Term(Box::new(e.to_string())))
+        }
+    }
 }
 
 rustler::init!("Elixir.Cairo.CairoVM", [cairo_vm_runner]);
